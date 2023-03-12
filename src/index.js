bf72d158771d76e9986d9cfb0f1526771512f565
haihung <hungnh12062000@gmail.com>
2023-03-12 23:24:04 +0700

    install pretter, lint-staged, husky

--- src/index.js
+++ src/index.js
@@ -5,41 +5,46 @@ const app = express();
 const port = 3000;
 
 // static file
-app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.static(path.join(__dirname, 'public')));
 
 // middleware
-app.use(express.urlencoded({
-     extended: true
-}));
-app.use(express.json());
+                    app.use(
+                         express.urlencoded({
+                              extended: true,
+                         }),
+                    );
+                    app.use(express.json());
 
 //template engine
 app.engine('hbs', handlebars.engine({
-     extname: '.hbs'
-}
-));
+          extname: '.hbs',
+     }),
+);
 app.set('view engine', 'hbs');
+
 app.set('views', path.join(__dirname, './resources/views'));
 
 // router
 app.get('/', (req, res) => {
-     res.render('home')
+     res.render('home');
 });
+
 app.get('/news', (req, res) => {
-     res.render('news')
+     res.render('news');
 });
+
 app.get('/search', (req, res) => {
      console.log(req.query);
      res.render('search');
 });
-app.post('/search', (req, res) => {
 
+app.post('/search', (req, res) => {
      console.log(req.body);
 
      res.send('search post');
 });
 
 // port
-app.listen(port, () => {
-     console.log(`Example app listening on port http://localhost:${port}`)
-})
+                    app.listen(port, () => {
+                         console.log(`Example app listening on port http://localhost:${port}`);
+                    });
