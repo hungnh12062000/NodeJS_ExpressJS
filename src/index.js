b5227341cad75fbd5e45631937c343bcd4909e40
haihung <hungnh12062000@gmail.com>
2023-03-12 22:54:11 +0700

    GET POST FORM

--- src/index.js
+++ src/index.js
@@ -1,10 +1,17 @@
-const path = require('path')
+const path = require('path');
 const express = require('express');
 const handlebars = require('express-handlebars');
 const app = express();
 const port = 3000;
 
+// static file
 app.use(express.static(path.join(__dirname, 'public')))
+
+// middleware
+app.use(express.urlencoded({
+     extended: true
+}));
+app.use(express.json());
 
 //template engine
 app.engine('hbs', handlebars.engine({
@@ -14,13 +21,25 @@ app.engine('hbs', handlebars.engine({
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, './resources/views'));
 
+// router
 app.get('/', (req, res) => {
      res.render('home')
-})
+});
 app.get('/news', (req, res) => {
      res.render('news')
-})
+});
+app.get('/search', (req, res) => {
+     console.log(req.query);
+     res.render('search');
+});
+app.post('/search', (req, res) => {
 
+     console.log(req.body);
+
+     res.send('search post');
+});
+
+// port
 app.listen(port, () => {
      console.log(`Example app listening on port http://localhost:${port}`)
 })
